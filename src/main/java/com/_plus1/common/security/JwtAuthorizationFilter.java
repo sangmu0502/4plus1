da97da0437453be37c6955d9dd4b46df90c713ce
Jeongha Im <yimjh3205@gmail.com>
2026-01-05 20:54:35 +0900

    ✨ feat: 프로필 조회 기능 구현
    - JwtAuthorizationFilter 구현
    - @AuthenticationPrincipal을 사용한 현재 로그인 유저 정보 추출 로직 적용
    - SecurityConfig 내 인가 필터 등록 및 접근 권한 설정

---
+++ src/main/java/com/_plus1/common/security/JwtAuthorizationFilter.java
@@ -0,0 +1,44 @@
+package com._plus1.common.security;
+
+import com._plus1.domain.user.service.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // 1. 헤더에서 토큰 꺼내기
+        String token = jwtUtil.getJwtFromHeader(request);
+
+        if (token != null) {
+            // 2. 토큰 검증
+            if (jwtUtil.verifyToken(token)) {
+                // 3. 토큰에서 유저 정보(nickname) 추출
+                String nickname = jwtUtil.getUserInfoFromToken(token);
+
+                // 4. 인증 객체 생성 및 Context에 저장
+                UserDetails userDetails = userDetailsService.loadUserByUsername(nickname);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
