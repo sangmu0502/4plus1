334210125ac3d4084e0bba96fe7369c4ae2c760d
Jeongha Im <yimjh3205@gmail.com>
2026-01-05 20:53:03 +0900

    Reapply "✨ feat: 회원가입 기능 구현"
    This reverts commit b091a8a0a1d98a3cd1f0f98cc630cdff319d6963.

--- src/main/java/com/_plus1/common/entity/User.java
+++ src/main/java/com/_plus1/common/entity/User.java
@@ -13,7 +13,7 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
@@ -22,21 +22,20 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nickname;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String phoneNumber;
 
     @Column(nullable = false)
     private boolean isDeleted = false;
 
-    public User(String email, String username, String password, String nickname, String phoneNumber, boolean isDeleted) {
+    public User(String email, String username, String password, String nickname, String phoneNumber) {
         this.email = email;
         this.username = username;
         this.password = password;
         this.nickname = nickname;
         this.phoneNumber = phoneNumber;
-        this.isDeleted = isDeleted;
     }
 }
--- src/main/java/com/_plus1/domain/user/repository/UserRepository.java
+++ src/main/java/com/_plus1/domain/user/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com._plus1.domain.user.repository;
 
-public interface UserRepository {
+import com._plus1.common.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
 }
