2b5f29ce56f3e50ff0a8601f1d37f53a9b723846
JH319 <imjh3205@gmail.com>
2026-01-06 17:37:21 +0900

    Update JwtAuthorizationFilter.java

--- src/main/java/com/_plus1/common/security/JwtAuthorizationFilter.java
+++ src/main/java/com/_plus1/common/security/JwtAuthorizationFilter.java
@@ -22,14 +22,6 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        String path = request.getRequestURI();
-
-        // 로그인 및 회원가입 경로는 토큰 검사 로직을 타지 않고 바로 다음 필터로 넘김
-        if (path.startsWith("/api/auth/login") || path.startsWith("/api/me/signup")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         // 헤더에서 토큰 꺼내기
         String token = jwtUtil.getJwtFromHeader(request);
 
@@ -47,5 +39,13 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    // 로그인 및 회원가입 경로는 토큰 검사 로직을 타지 않고 바로 다음 필터로 넘김
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request){
+        String path = request.getRequestURI();
+        return path.startsWith("/api/auth")
+                || path.equals("/api/me/signup");
     }
 }
