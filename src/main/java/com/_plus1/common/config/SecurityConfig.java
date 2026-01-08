61f41e021374590307aad147c1e7eb3957dfc68f
Jinsoo <milestone9701@gmail.com>
2026-01-08 20:13:11 +0900

    🐛 fix: SecurityConfig 누락 수정
    ### 생성됨
    
    ### 수정됨
    - SecurityConfig.java
    
    #### 내용
    검색 품질 개선 : 정렬, 일치, 우선도 개념.
    
    #### 비고

--- src/main/java/com/_plus1/common/config/SecurityConfig.java
+++ src/main/java/com/_plus1/common/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
         // 로그인, 회원가입은 인증 없이 허용. 그 외의 모든 요청은 인증 필요
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/login", "/api/me/signup", "/api/albums/**", "/api/search/**", "/api/songs/top", "/api/songs/top/v2", "/api/songs/korea/new", "/api/songs/global/new", "/api/songs/korea", "/api/songs/global").permitAll() // 로그인, 회원가입은 인증 없이 허용
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
         );
 
         // JWT 인가 필터 추가 (UsernamePasswordAuthenticationFilter 이전에 실행되도록)
